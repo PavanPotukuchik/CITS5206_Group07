b110ba8887f425f5c318a08c092a27cd95550242
jankirangani <jankirangani31@gmail.com>
2024-09-09 18:08:36 +0800

    add client backend working-janki

---
+++ static/js/add-client.js
@@ -0,0 +1,15 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+
+// example create data
+const data = {
+    "clientName": "test",
+    "clientEmail": "test@example.com",
+    "contactNumber": 123,
+    "companyName": "test",
+    "password": "test"
+};
+
+const record = await pb.collection('clients').create(data);
