d8c61895c6eaccf81a55fca431e4a2c3a78949c7
Chris Wang <kkglovemy@gmail.com>
2024-10-05 14:22:29 +0800

    deploy

--- app.py
+++ app.py
@@ -4,16 +4,32 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/admin/')
-def proxy_pocketbase_admin():
-
-    resp = requests.get('http://localhost:8090/_/', headers=request.headers, stream=True)
-
-    return Response(
-        resp.iter_content(chunk_size=10 * 1024),
-        content_type=resp.headers['Content-Type'],
-        status=resp.status_code,
+@app.route('/pocketbase/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy_to_pocketbase(path):
+    url = f'http://localhost:8090/{path}'
+    
+    headers = {key: value for (key, value) in request.headers if key != 'Host'}
+    
+    resp = requests.request(
+        method=request.method,
+        url=url,
+        headers=headers,
+        data=request.get_data(),
+        cookies=request.cookies, 
+        allow_redirects=False,
+        stream=True
     )
+    
+    response = Response(
+        resp.iter_content(chunk_size=10 * 1024),
+        content_type=resp.headers.get('Content-Type'),
+        status=resp.status_code
+    )
+    
+    for cookie in resp.cookies:
+        response.set_cookie(cookie.name, cookie.value, **cookie.get_dict())
+    
+    return response
     
 @app.route('/')
 def index():
