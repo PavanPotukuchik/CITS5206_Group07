f12bd8c3917a91772c06ddf8f0aae4457d1b805b
Janki Rangani <jankirangani31@gmail.com>
2024-10-09 19:34:22 +0800

    add project changed to running state

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-POCKETBASE_URL = "http://127.0.0.1:8090"
+POCKETBASE_URL = "https://five206pocketbase.onrender.com/"
 
 @app.route('/')
 def index():
@@ -28,46 +28,8 @@ def add_client():
 def project_list():
     return render_template('project-list.html', active_page='project')
 
-@app.route('/project-form')   #,methods=['GET', 'POST']
+@app.route('/project-form') 
 def project_form():
-    # if request.method == 'POST':
-    #     # Get the project title from the form
-    #     project_title = request.form.get('projectTitle')
-
-    #     if not project_title:
-    #         return "Project title is required", 400 
-
-    #     # Create the project in PocketBase
-    #     try:
-    #         # API endpoint for creating a new project in PocketBase 
-    #         url = f"{POCKETBASE_URL}/api/collections/projects/records"
-
-    #         # Data to send to PocketBase
-    #         data = {
-    #             "title": project_title
-    #         }
-
-    #         # Make the POST request to create the project
-    #         response = request.post(url, json=data)
-
-    #         # Check if the request was successful
-    #         if response.status_code == 200:
-    #             # Parse the response JSON
-    #             project_data = response.json()
-
-    #             # Retrieve the automatically generated project ID from the response
-    #             project_id = project_data['id']  # PocketBase returns the new record's ID
-
-    #             # Redirect to the next page (question form), passing the project_id in the URL
-    #             return redirect(url_for('question_form', project_id=project_id))
-    #         else:
-    #             # Handle error response
-    #             return f"Error creating project: {response.text}", response.status_code
-
-    #     except Exception as e:
-    #         return f"An error occurred while creating the project: {str(e)}"
-
-    # Render the project creation form if the method is GET
     return render_template('project-form.html', active_page='project-form')
 
 
@@ -100,8 +62,8 @@ def modify_settings():
     return render_template('modify-settings.html', active_page='settings')
 
 @app.route('/question-form')
-def question_form():   #project_id
-    return render_template('question-form.html', active_page = 'project-form') #,project_id=project_id
+def question_form():
+    return render_template('question-form.html', active_page = 'project-form')
 
 @app.errorhandler(404)
 def page_not_found(e): 
